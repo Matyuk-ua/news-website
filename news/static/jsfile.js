2bc671c8bf87dcd50d4989dd5f6e19452ff5c1d8
Matyuk <124920731+Matyuk-ua@users.noreply.github.com>
2023-12-25 13:49:22 +0200

    Add files via upload

--- news/static/jsfile.js
+++ news/static/jsfile.js
@@ -1,8 +1,6 @@
 
-// 
 window.onload = function TitleDivSize() {
   var div = document.getElementsByClassName('title_space');
   var contentHeight = div.scrollHeight;
   div.style.height = contentHeight + 'px';
 };
-
